2dfb00b1450adaf36d56474cedeb4cfc9120e75d
JuanPaSierraR <sierrajuanpablo7gmail.com>
2021-08-11 08:56:31 -0500

    Finalizado el crud de la tabla proveedor

---
+++ ASP2236903/Controllers/ProveedorController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASP2236903.Models;
+
+namespace ASP2236903.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new invent2021Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new invent2021Entities())
+            {
+                var FindProveedor = db.proveedor.Find(id);
+                return View(FindProveedor);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    var FindProveedor = db.proveedor.Find(id);
+                    db.proveedor.Remove(FindProveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    proveedor FindProveedor = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(FindProveedor);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(proveedor editProved)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    proveedor user = db.proveedor.Find(editProved.id);
+                    user.nombre = editProved.nombre;
+                    user.direccion = editProved.direccion;
+                    user.telefono = editProved.telefono;
+                    user.nombre_contacto = editProved.nombre_contacto;
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+            }
+        }
+    }
+}
