7a6cd2639e77d802efc912c515834a52bfbb9c9c
JuanPaSierraR <sierrajuanpablo7gmail.com>
2021-08-11 09:10:16 -0500

    Finalizado el crud de la tabla cliente

--- ASP2236903/Controllers/ClienteController.cs
+++ ASP2236903/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASP2236903.Models;
 
 namespace ASP2236903.Controllers
 {
@@ -11,7 +12,111 @@ namespace ASP2236903.Controllers
         // GET: Cliente
         public ActionResult Index()
         {
+            using (var db = new invent2021Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new invent2021Entities())
+            {
+                var FindCliente = db.cliente.Find(id);
+                return View(FindCliente);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    var FindCliente = db.cliente.Find(id);
+                    db.cliente.Remove(FindCliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    cliente FindCliente = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(FindCliente);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editCliente)
+        {
+            try
+            {
+                using (var db = new invent2021Entities())
+                {
+                    cliente user = db.cliente.Find(editCliente.id);
+                    user.nombre = editCliente.nombre;
+                    user.documento = editCliente.documento;
+                    user.email = editCliente.email;
+                    
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+            }
         }
     }
 }
